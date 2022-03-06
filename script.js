7f8cf340d3f9d9cb70927d05f2017d2d7785d6ee
thulerjoao <thuler_lima@hotmail.com>
2022-03-06 19:32:29 -0300

    commit_v1.1

--- script.js
+++ script.js
@@ -32,13 +32,13 @@ const tempo = (ms) => {
     }
   }
 
-//a função 'exibirResultados' receber um array de objetos e retorna uma tabela sem poluição como no 'console.table()'
-function exibirResultados(input) {
+//a função 'exibirTabelaNova' recebe um array de objetos e retorna uma tabela sem poluição como no 'console.table()'
+function exibirTabelaNova(input) {
     
-    const ts = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } })
-    const logger = new Console({ stdout: ts })
+    const ts = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } });
+    const logger = new Console({ stdout: ts });
     logger.table(input)
-    const table = (ts.read() || '').toString()
+    const table = (ts.read() || '').toString();
     let result = '';
     for (let row of table.split(/[\r\n]+/)) {
       let r = row.replace(/[^┬]*┬/, '┌');
@@ -85,17 +85,50 @@ const votacao = (autorizacao, voto) => {
         console.clear();
         console.log((`Voto ${condicao}!\n`).toUpperCase());
         console.log("TABELA DE VOTAÇÃO");
-        exibirResultados(tabelaVotos);
+        exibirTabelaNova(tabelaVotos);
         voto = parseInt(prompt("Digite o voto de acordo com a tabela [1 a 5]: "));
         while(isNaN(voto) || voto < 1 || voto > 5){ //validação do voto
             console.clear();  
             console.log("!!!ATENÇÃO!!!");
-            exibirResultados(tabelaVotos);
+            exibirTabelaNova(tabelaVotos);
             voto = parseInt(prompt("Digite o voto de acordo com a tabela [1 a 5]: "));           
   }
         contabilizar[voto-1][1] ++;
     }
 
+}
+//função exibirResultados para finalizar o programa:
+
+const exibirResultados = () =>{ 
+  
+//const result serve para tranformar o array 'contabilizar' em um array de objetos, afim servir como entrada a função 'exibirTabelaNova'
+const result = [
+  { Candidato: "Candidato 1", Votos: contabilizar[0][1]},
+  { Candidato: "Candidato 2", Votos: contabilizar[1][1]},
+  { Candidato: "Candidato 3", Votos: contabilizar[2][1]},
+  { Candidato: "Nulos", Votos: contabilizar[3][1]},
+  { Candidato: "Brancos", Votos: contabilizar[4][1]}
+];
+
+console.clear();
+console.log("Confira o resultado da votação: ");
+exibirTabelaNova(result);
+
+result.splice(3,2);//retirando brancos e nulos do array para exclui-los da definição do vencedor
+
+//função que ordena o array result em ordem decrescente dos votos recebidos pelos candidatos(candidato na pos 0 recebeu mais votos.):
+result.sort(function (a, b) {
+  return a.Votos < b.Votos ? -1 : a.Votos > b.Votos ? 1 : 0;
+}).reverse();
+
+//estrutura que define o vencedor da votação:
+if(result[0].Votos == result[1].Votos && result[1].Votos == result[2].Votos){
+  console.log(`Os tres Candidatos empataram com ${result[0].Votos} voto(s).`);    
+}else if(result[0].Votos == result[1].Votos){
+  console.log(`O ${result[1].Candidato} e ${result[0].Candidato} empataram com ${result[0].Votos} voto(s).`);
+}else{
+    console.log(`O grande vencedor foi o ${result[0].Candidato} com ${result[0].Votos} voto(s)!`);
+}
 }
 
 let votarNovamente = 's';
@@ -105,7 +138,6 @@ while(votarNovamente == 'sim' || votarNovamente == 's' || votarNovamente == 1){
     votacao(autorizaVoto(idade), escolha);
     console.clear();
     nEleitor ++;   
-    
 
     votarNovamente = prompt(`Adicionar voto de novo eleitor? [s] ou [n]: `).toLowerCase();
     console.clear();
@@ -117,31 +149,4 @@ while(votarNovamente == 'sim' || votarNovamente == 's' || votarNovamente == 1){
         console.clear();
     }
 }
-
-//const result serve para tranformar o array 'contabilizar' em um array de objetos, afim servir como entrada a função 'exibirResultados'
-const result = [
-    { Candidato: "Candidato 1", Votos: contabilizar[0][1]},
-    { Candidato: "Candidato 2", Votos: contabilizar[1][1]},
-    { Candidato: "Candidato 3", Votos: contabilizar[2][1]},
-    { Candidato: "Nulos", Votos: contabilizar[3][1]},
-    { Candidato: "Brancos", Votos: contabilizar[4][1]}
-];
-
-console.clear();
-console.log("Confira resultado da votação: ");
-exibirResultados(result);
-
-result.splice(3,2);//retirando brancos e nulos do array para facilitar a definição do vencedor
-//função que ordena o array result em ordem decrescente dos votos recebidos pelos candidatos(candidato na pos 0 recebeu mais votos.):
-result.sort(function (a, b) {
-    return a.Votos < b.Votos ? -1 : a.Votos > b.Votos ? 1 : 0;
-  }).reverse();
-
-//estrutura que define o vencedor da votação:
-  if(result[0].Votos == result[1].Votos && result[1].Votos == result[2].Votos){
-    console.log(`Os tres Candidatos empataram com ${result[0].Votos} votos.`);    
-  }else if(result[0].Votos == result[1].Votos){
-    console.log(`O ${result[1].Candidato} e ${result[0].Candidato} empataram com ${result[0].Votos} votos.`);
-  }else{
-      console.log(`O grande vencedor foi o ${result[0].Candidato} com ${result[0].Votos} votos!`);
-  }
+exibirResultados();
